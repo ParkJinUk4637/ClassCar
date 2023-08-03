c2f1765dc2cc003d9343deedf6c6f30efffb540a
ParkJinUk4637 <noticepju@naver.com>
2023-08-04 02:21:28 +0900

    차량 상세 정보 페이지 내용 추가

--- lib/layouts/main_page/main_page/detail_car_page/detail_car_page.dart
+++ lib/layouts/main_page/main_page/detail_car_page/detail_car_page.dart
@@ -2,29 +2,68 @@ import 'package:flutter/material.dart';
 
 import '../../../../models/car.dart';
 
-class DetailCarPage extends StatefulWidget{
-  const DetailCarPage({Key? key, required this.car}): super(key: key);
+class DetailCarPage extends StatefulWidget {
+  const DetailCarPage({Key? key, required this.car}) : super(key: key);
 
   final Car car;
+
   @override
   State<StatefulWidget> createState() => _DetailCarPage();
 }
 
-class _DetailCarPage extends State<DetailCarPage>{
+class _DetailCarPage extends State<DetailCarPage> {
   late Car car = widget.car;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text("${car.description}"),
-          ],
-        ) ,
-      )
+      body: ListView(
+        controller: null,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                "https://taegon.kim/wp-content/uploads/2018/05/image-5.png",
+                width: 500,
+                fit: BoxFit.fitHeight,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "${car.carModel}",
+                      textAlign: TextAlign.start,
+                    ),
+                    Text(
+                      "${car.score} (${car.sharedCount}회 대여)",
+                    ),
+                    const SizedBox(height: 16.0),
+                    const Text("대여 날짜"),
+                    const Text("날짜 설정"),
+                    const SizedBox(height: 16.0),
+                    const Text("대여&반납 위치"),
+                    Text("${car.carLocation}"),
+                    const SizedBox(height: 16.0),
+                    const Text("환불 정책"),
+                    const Text("환불 정책 내용"),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    const Text("차량 추가 정보"),
+                    Text("좌석 : ${car.seats}인석"),
+                    Text("기름 종류 : ${car.oilType}"),
+                    Text("연식 : ${car.years}"),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
-
 }

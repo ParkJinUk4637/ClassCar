2d17687336f26fcd16887132266ab1335ee4e5df
ParkJinUk4637 <noticepju@naver.com>
2023-08-09 19:54:52 +0900

    복구용

--- lib/models/car.dart
+++ lib/models/car.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:uuid/uuid.dart';
-
+/// uid : 0
 /// isExhibit : false
 /// carNumber : "carNumber"
 /// carModel : "carModel"
@@ -19,30 +17,29 @@ import 'package:uuid/uuid.dart';
 /// cancelPolicyPercent : 60
 /// oilType : "oilType"
 /// description : "description"
-/// createdAt :
 
 class Car {
-  Car(
-      {String? uuid,
-      bool? isExhibit,
-      String? carNumber,
-      String? carModel,
-      String? carType,
-      String? carDrivetrain,
-      String? maker,
-      num? seats,
-      num? carGasMil,
-      String? carLocation,
-      bool? fwd,
-      String? years,
-      num? score,
-      num? sharedCount,
-      num? sharingPrice,
-      num? cancelPolicyDate,
-      num? cancelPolicyPercent,
-      String? oilType,
-      String? description,
-      Timestamp? createdAt}) {
+  Car({
+      num? uid, 
+      bool? isExhibit, 
+      String? carNumber, 
+      String? carModel, 
+      String? carType, 
+      String? carDrivetrain, 
+      String? maker, 
+      num? seats, 
+      num? carGasMil, 
+      String? carLocation, 
+      bool? fwd, 
+      String? years, 
+      num? score, 
+      num? sharedCount, 
+      num? sharingPrice, 
+      num? cancelPolicyDate, 
+      num? cancelPolicyPercent, 
+      String? oilType, 
+      String? description,}){
+    _uid = uid;
     _isExhibit = isExhibit;
     _carNumber = carNumber;
     _carModel = carModel;
@@ -61,11 +58,10 @@ class Car {
     _cancelPolicyPercent = cancelPolicyPercent;
     _oilType = oilType;
     _description = description;
-    _createdAt = createdAt;
-  }
+}
 
   Car.fromJson(dynamic json) {
-    _uuid = json['uuid'];
+    _uid = json['uid'];
     _isExhibit = json['isExhibit'];
     _carNumber = json['carNumber'];
     _carModel = json['carModel'];
@@ -84,39 +80,8 @@ class Car {
     _cancelPolicyPercent = json['cancelPolicyPercent'];
     _oilType = json['oilType'];
     _description = json['description'];
-    _createdAt = json['createdAt'];
   }
-
-  factory Car.fromFirestore(
-    DocumentSnapshot<Map<String, dynamic>> snapshot,
-    SnapshotOptions? options,
-  ) {
-    final data = snapshot.data();
-    return Car(
-      uuid: data?['uuid'],
-      isExhibit: data?['isExhibit'],
-      carNumber: data?['carNumber'],
-      carModel: data?['carModel'],
-      carType: data?['carType'],
-      carDrivetrain: data?['carDrivetrain'],
-      maker: data?['maker'],
-      seats: data?['seats'],
-      carGasMil: data?['carGasMil'],
-      carLocation: data?['carLocation'],
-      fwd: data?['FWD'],
-      years: data?['years'],
-      score: data?['score'],
-      sharedCount: data?['sharedCount'],
-      sharingPrice: data?['sharingPrice'],
-      cancelPolicyDate: data?['cancelPolicyDate'],
-      cancelPolicyPercent: data?['cancelPolicyPercent'],
-      oilType: data?['oilType'],
-      description: data?['description'],
-      createdAt: data?['createdAt'],
-    );
-  }
-
-  String? _uuid;
+  num? _uid;
   bool? _isExhibit;
   String? _carNumber;
   String? _carModel;
@@ -135,97 +100,68 @@ class Car {
   num? _cancelPolicyPercent;
   String? _oilType;
   String? _description;
-  Timestamp? _createdAt;
-
-  Car copyWith({
-    String? uuid,
-    num? uid,
-    bool? isExhibit,
-    String? carNumber,
-    String? carModel,
-    String? carType,
-    String? carDrivetrain,
-    String? maker,
-    num? seats,
-    num? carGasMil,
-    String? carLocation,
-    bool? fwd,
-    String? years,
-    num? score,
-    num? sharedCount,
-    num? sharingPrice,
-    num? cancelPolicyDate,
-    num? cancelPolicyPercent,
-    String? oilType,
-    String? description,
-    DateTime? createdAt,
-  }) =>
-      Car(
-        uuid: uuid ?? _uuid,
-        isExhibit: isExhibit ?? _isExhibit,
-        carNumber: carNumber ?? _carNumber,
-        carModel: carModel ?? _carModel,
-        carType: carType ?? _carType,
-        carDrivetrain: carDrivetrain ?? _carDrivetrain,
-        maker: maker ?? _maker,
-        seats: seats ?? _seats,
-        carGasMil: carGasMil ?? _carGasMil,
-        carLocation: carLocation ?? _carLocation,
-        fwd: fwd ?? _fwd,
-        years: years ?? _years,
-        score: score ?? _score,
-        sharedCount: sharedCount ?? _sharedCount,
-        sharingPrice: sharingPrice ?? _sharingPrice,
-        cancelPolicyDate: cancelPolicyDate ?? _cancelPolicyDate,
-        cancelPolicyPercent: cancelPolicyPercent ?? _cancelPolicyPercent,
-        oilType: oilType ?? _oilType,
-        description: description ?? _description,
-        createdAt: _createdAt ?? _createdAt,
-      );
-
-  String? get uuid => _uuid;
-
+Car copyWith({  num? uid,
+  bool? isExhibit,
+  String? carNumber,
+  String? carModel,
+  String? carType,
+  String? carDrivetrain,
+  String? maker,
+  num? seats,
+  num? carGasMil,
+  String? carLocation,
+  bool? fwd,
+  String? years,
+  num? score,
+  num? sharedCount,
+  num? sharingPrice,
+  num? cancelPolicyDate,
+  num? cancelPolicyPercent,
+  String? oilType,
+  String? description,
+}) => Car(  uid: uid ?? _uid,
+  isExhibit: isExhibit ?? _isExhibit,
+  carNumber: carNumber ?? _carNumber,
+  carModel: carModel ?? _carModel,
+  carType: carType ?? _carType,
+  carDrivetrain: carDrivetrain ?? _carDrivetrain,
+  maker: maker ?? _maker,
+  seats: seats ?? _seats,
+  carGasMil: carGasMil ?? _carGasMil,
+  carLocation: carLocation ?? _carLocation,
+  fwd: fwd ?? _fwd,
+  years: years ?? _years,
+  score: score ?? _score,
+  sharedCount: sharedCount ?? _sharedCount,
+  sharingPrice: sharingPrice ?? _sharingPrice,
+  cancelPolicyDate: cancelPolicyDate ?? _cancelPolicyDate,
+  cancelPolicyPercent: cancelPolicyPercent ?? _cancelPolicyPercent,
+  oilType: oilType ?? _oilType,
+  description: description ?? _description,
+);
+  num? get uid => _uid;
   bool? get isExhibit => _isExhibit;
-
   String? get carNumber => _carNumber;
-
   String? get carModel => _carModel;
-
   String? get carType => _carType;
-
   String? get carDrivetrain => _carDrivetrain;
-
   String? get maker => _maker;
-
   num? get seats => _seats;
-
   num? get carGasMil => _carGasMil;
-
   String? get carLocation => _carLocation;
-
   bool? get fwd => _fwd;
-
   String? get years => _years;
-
   num? get score => _score;
-
   num? get sharedCount => _sharedCount;
-
   num? get sharingPrice => _sharingPrice;
-
   num? get cancelPolicyDate => _cancelPolicyDate;
-
   num? get cancelPolicyPercent => _cancelPolicyPercent;
-
   String? get oilType => _oilType;
-
   String? get description => _description;
-
-  Timestamp? get createdAt => _createdAt;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
-    map['uuid'] = _uuid;
+    map['uid'] = _uid;
     map['isExhibit'] = _isExhibit;
     map['carNumber'] = _carNumber;
     map['carModel'] = _carModel;
@@ -244,7 +180,7 @@ class Car {
     map['cancelPolicyPercent'] = _cancelPolicyPercent;
     map['oilType'] = _oilType;
     map['description'] = _description;
-    map['createdAt'] = _createdAt;
     return map;
   }
+
 }

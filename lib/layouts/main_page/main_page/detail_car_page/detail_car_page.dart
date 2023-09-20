fb3ba83fd06f91c1f22be8804d32651f558f8d67
ParkJinUk4637 <noticepju@naver.com>
2023-09-20 23:33:11 +0900

    결제 구현 완료

--- lib/layouts/main_page/main_page/detail_car_page/detail_car_page.dart
+++ lib/layouts/main_page/main_page/detail_car_page/detail_car_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_time_picker_spinner/flutter_time_picker_spinner.dart';
+import 'package:get/get.dart';
 import 'package:my_classcar/layouts/main_page/app_bar.dart';
 import '../../../../models/car_info_model.dart';
 import '../../../../models/rent.dart';
@@ -71,13 +72,14 @@ class _DetailCarPage extends State<DetailCarPage> {
           Expanded(
             flex: 3,
             child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(textAlign: TextAlign.left, "${car.sharingPrice}원/일"),
-              Text(textAlign: TextAlign.left, "합계요금 : $totalPrice"),
-            ],
-          ),),
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(textAlign: TextAlign.left, "${car.sharingPrice}원/일"),
+                Text(textAlign: TextAlign.left, "합계요금 : $totalPrice"),
+              ],
+            ),
+          ),
 
           // SizedBox(width: MediaQuery.of(context).size.width / 460 * 200),
           // TextButton(
@@ -100,92 +102,73 @@ class _DetailCarPage extends State<DetailCarPage> {
           Expanded(
             flex: 1,
             child: TextButton(
-            style: TextButton.styleFrom(
-              padding: const EdgeInsets.all(12.0),
-              foregroundColor: Colors.white,
-              backgroundColor: Theme.of(context).focusColor,
-              textStyle: const TextStyle(fontSize: 16),
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.all(12.0),
+                foregroundColor: Colors.white,
+                backgroundColor: Theme.of(context).focusColor,
+                textStyle: const TextStyle(fontSize: 16),
+              ),
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => SecondRoute()),
+                // );
+
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text("대여"),
+                        content: const Text("대여 하시겠습니까?"),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text("취소"),
+                          ),
+                          TextButton(
+                            onPressed: () async {
+                              Navigator.of(context).pop();
+                              payCredit(totalPrice);
+                            },
+                            child: const Text("확인"),
+                          )
+                        ],
+                      );
+                    });
+              },
+              child: const Text("대여하기"),
             ),
-            onPressed: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(builder: (context) => SecondRoute()),
-              // );
-
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: const Text("대여"),
-                      content: const Text("대여 하시겠습니까?"),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text("취소"),
-                        ),
-                        TextButton(
-                          onPressed: () async {
-                            final Rent rent = Rent(
-                                carUid: car.docID,
-                                driverUid: widget.driverUid,
-                                rentalCost: 100000,
-                                // 작업필요
-                                rentalEndTime: Timestamp.fromDate(endDate),
-                                // 작업필요
-                                rentalStartTime: Timestamp.fromDate(startDate),
-                                // 작업필요
-                                requestDate: Timestamp.now(),
-                                situation: "수락대기",
-                                ownerUid: car.uuid);
-                            await db.collection('Rent').add(rent.toJson());
-                            if (!mounted) return;
-                            Navigator.of(context).pop();
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => const MainLayout(
-                                  index: 1,
-                                ),
-                              ),
-                            );
-                          },
-                          child: const Text("확인"),
-                        )
-                      ],
-                    );
-                  });
-            },
-            child: const Text("대여하기"),
-          ),)
-
+          )
         ],
       ),
     );
   }
 
-  Widget dateTimeListTile(){
+  Widget dateTimeListTile() {
     return ListTile(
       onTap: () async {
         final selectStart = await showDatePicker(
           context: context,
           initialDate: startDate,
           firstDate: DateTime.now(),
-          lastDate: DateTime(DateTime.now().year,
-              DateTime.now().month + 1, DateTime.now().day-1),
+          lastDate: DateTime(DateTime.now().year, DateTime.now().month + 1,
+              DateTime.now().day - 1),
         );
 
         if (!mounted) return;
         final selectEnd = await showDatePicker(
           context: context,
-          initialDate : DateTime(selectStart!.year,selectStart.month,selectStart.day+1),
-          firstDate: DateTime(selectStart.year,selectStart.month,selectStart.day+1),
-          lastDate: DateTime(DateTime.now().year,
-              DateTime.now().month + 1, DateTime.now().day),
+          initialDate: DateTime(
+              selectStart!.year, selectStart.month, selectStart.day + 1),
+          firstDate: DateTime(
+              selectStart.year, selectStart.month, selectStart.day + 1),
+          lastDate: DateTime(DateTime.now().year, DateTime.now().month + 1,
+              DateTime.now().day),
         );
 
-        if (selectEnd!=null &&
-            selectEnd.day != selectStart.day) {
+        if (selectEnd != null && selectEnd.day != selectStart.day) {
           setState(() {
             startDate = selectStart;
             endDate = selectEnd;
@@ -206,8 +189,7 @@ class _DetailCarPage extends State<DetailCarPage> {
                             style: TextButton.styleFrom(
                               padding: const EdgeInsets.all(12.0),
                               foregroundColor: Colors.white,
-                              backgroundColor:
-                              Theme.of(context).focusColor,
+                              backgroundColor: Theme.of(context).focusColor,
                               textStyle: const TextStyle(fontSize: 16),
                             ),
                             onPressed: () {
@@ -226,22 +208,10 @@ class _DetailCarPage extends State<DetailCarPage> {
       title: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-              '대여 : ${startDate.year}년 ${startDate.month}월 ${startDate.day}일 '
-                  '${startDate.weekday == 1 ? '월'
-                  : startDate.weekday == 2 ? '화'
-                  : startDate.weekday==3? '수'
-                  : startDate.weekday==4? '목'
-                  : startDate.weekday==5? '금'
-                  : startDate.weekday==6? '토' : '일'}요일 ${startDate.hour}시 ${startDate.minute}분'),
-          Text(
-              '반납 : ${endDate.year}년 ${endDate.month}월 ${endDate.day}일 '
-                  '${endDate.weekday == 1 ? '월'
-                  : endDate.weekday == 2 ? '화'
-                  : endDate.weekday==3? '수'
-                  : endDate.weekday==4? '목'
-                  : endDate.weekday==5? '금'
-                  : endDate.weekday==6? '토' : '일'}요일 ${endDate.hour}시 ${endDate.minute}분')
+          Text('대여 : ${startDate.year}년 ${startDate.month}월 ${startDate.day}일 '
+              '${startDate.weekday == 1 ? '월' : startDate.weekday == 2 ? '화' : startDate.weekday == 3 ? '수' : startDate.weekday == 4 ? '목' : startDate.weekday == 5 ? '금' : startDate.weekday == 6 ? '토' : '일'}요일 ${startDate.hour}시 ${startDate.minute}분'),
+          Text('반납 : ${endDate.year}년 ${endDate.month}월 ${endDate.day}일 '
+              '${endDate.weekday == 1 ? '월' : endDate.weekday == 2 ? '화' : endDate.weekday == 3 ? '수' : endDate.weekday == 4 ? '목' : endDate.weekday == 5 ? '금' : endDate.weekday == 6 ? '토' : '일'}요일 ${endDate.hour}시 ${endDate.minute}분')
         ],
       ),
       trailing: const Icon(Icons.arrow_forward_ios),
@@ -282,10 +252,85 @@ class _DetailCarPage extends State<DetailCarPage> {
           endDate = DateTime(
               endDate.year, endDate.month, endDate.day, time.hour, time.minute);
 
-          totalPrice = (car.sharingPrice! * int.parse(endDate.difference(startDate).inDays.toString()));
+          totalPrice = (car.sharingPrice! *
+              int.parse(endDate.difference(startDate).inDays.toString()));
         });
       },
     );
+  }
+
+  Future<void> payCredit(int totalPrice) async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    await FirebaseFirestore.instance
+        .collection('userINFO')
+        .where('email', isEqualTo: user?.email)
+        .get()
+        .then((value) {
+      final data = value.docs.first.data();
+      final credit = data['credit'];
+
+      if (credit < totalPrice) {
+        print("크레딧 부족");
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("크레딧 부족"),
+                content: Text("크레딧이 ${(credit - totalPrice)*-1}원 부족합니다"),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("확인"),
+                  )
+                ],
+              );
+            });
+      } else {
+        print("요청 시작");
+        FirebaseFirestore.instance
+            .collection('userINFO')
+            .doc(value.docs.first.id)
+            .update({"credit": credit - totalPrice}).then((value) async {
+          final Rent rent = Rent(
+              carUid: car.docID,
+              driverUid: widget.driverUid,
+              rentalCost: totalPrice,
+              // 작업필요
+              rentalEndTime: Timestamp.fromDate(endDate),
+              // 작업필요
+              rentalStartTime: Timestamp.fromDate(startDate),
+              // 작업필요
+              requestDate: Timestamp.now(),
+              situation: "수락대기",
+              ownerUid: car.uuid);
+          await db.collection('Rent').add(rent.toJson());
+          if (!mounted) return;
+          setState(() {
+            Get.offAll(() => const MainLayout(index: 1,));
+          });
+        }, onError: (e) {
+          print(e);
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text("크레딧 부족"),
+                  content: Text("크레딧이 ${(credit - totalPrice)*-1}원 부족합니다"),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("확인"),
+                    )
+                  ],
+                );
+              });
+        });
+      }
+    });
   }
 
   Widget _info() {
@@ -323,7 +368,7 @@ class _DetailCarPage extends State<DetailCarPage> {
   }
 
   @override
-  void initState(){
+  void initState() {
     totalPrice = car.sharingPrice!;
     super.initState();
   }
